d08731185c9305465b21285f352481aa4e0b8ba2
Aviad Blyacher <aviadbl05@gmail.com>
2022-04-03 20:10:03 +0300

    updated python

--- Python Script/venv/Client.py
+++ Python Script/venv/Client.py
@@ -1,6 +1,6 @@
 import socket
-SERVER_IP = "---"
-SERVER_PORT = 00
+SERVER_IP = "77.137.73.138"
+SERVER_PORT = 8200
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,3 +11,7 @@ sock.connect(server_address)
 
 msg = "Hello"
 sock.sendall(msg.encode())
+
+server_msg = sock.recv(1024)
+server_msg = server_msg.decode()
+print(server_msg)
