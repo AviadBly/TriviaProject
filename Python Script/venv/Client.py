9602c76242da92c1d45d5cbe90b29fc422af6597
Aviad Blyacher <aviadbl05@gmail.com>
2022-04-04 19:14:35 +0300

    added json

--- Python Script/venv/Client.py
+++ Python Script/venv/Client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 SERVER_IP = "77.137.73.138"
 SERVER_PORT = 8200
 
@@ -9,9 +10,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (SERVER_IP, SERVER_PORT)
 sock.connect(server_address)
 
-msg = "Hello"
-sock.sendall(msg.encode())
-
+login_msg = {"username":"user1","password":"1234"}
+signup_msg={"username": "user1", "password": "1234", "mail":"user1@gmail.com"}
+data=json.dumps(signup_msg)          #USE EITHER LOGIN_MSG OR SIGNUP MSG
+sock.sendall(data.encode())
+server_msg = sock.recv(1024)
+server_msg = server_msg.decode()
+print(server_msg)
+data=json.dump(login_msg)
+sock.sendall(data.encode())
 server_msg = sock.recv(1024)
 server_msg = server_msg.decode()
 print(server_msg)
