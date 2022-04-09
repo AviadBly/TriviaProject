33fcb3ea04d8f16730cab108c89c86841504c216
shahar m <shaharmar1@kramim.ort.org.il>
2022-04-09 14:43:02 +0300

    client works with signUp and login msg

--- Python Script/venv/Client.py
+++ Python Script/venv/Client.py
@@ -3,8 +3,8 @@ import json
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8200
 
-SIGN_UP_CODE = 100
-LOGIN_CODE = 101
+SIGN_UP_CODE = 10
+LOGIN_CODE = 20
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -17,13 +17,16 @@ login_dict = {"username":"user1","password":"1234"}
 signup_dict = {"username": "user1", "password": "1234", "mail":"user1@gmail.com"}
 
 
-signup_msg = bytes(SIGN_UP_CODE) + bytes(len(signup_dict)) + str(signup_dict).encode()
-login_msg = bytes(LOGIN_CODE) + bytes(len(login_dict)) + str(login_dict).encode()
+signup_msg = chr(SIGN_UP_CODE).encode() + len(str(signup_dict)).to_bytes(4, byteorder='big') + str(signup_dict).encode()
+login_msg = chr(LOGIN_CODE).encode() + len(str(login_dict)).to_bytes(4, byteorder='big') + str(login_dict).encode()
 
+print(signup_msg, "length: " + str(len(signup_msg)))
 
-data = json.dumps(signup_msg)          #USE EITHER LOGIN_MSG OR SIGNUP MSG
-sock.sendall(data.encode())
+# data = json.dumps(signup_msg)          #USE EITHER LOGIN_MSG OR SIGNUP MSG
 
 server_msg = sock.recv(1024)
 server_msg = server_msg.decode()
+
+sock.sendall(signup_msg)
+
 print(server_msg)
