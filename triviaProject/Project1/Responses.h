9ca5ead4c71d032a443d7c93c2b620d429447d7a
shahar m <shaharmar1@kramim.ort.org.il>
2022-04-17 14:10:35 +0300

    Adding responses

--- triviaProject/Project1/Responses.h
+++ triviaProject/Project1/Responses.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "Room.h"
 
 struct Response {
 	unsigned int status;
@@ -17,4 +19,32 @@ struct SignUpResponse : public Response {
 
 struct ErrorResponse {
 	std::string message;
+};
+
+struct LogoutResponse {
+
+};
+
+struct GetRoomResponse : public Response {
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse {
+	std::vector<std::string> players;
+};
+
+struct getHighScoreResponse : public Response {
+	std::vector<std::string> statistics;
+};
+
+struct getPersonalStatsResponse : public Response {
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse : public Response {
+
+};
+
+struct CreateRoomResponse : public Response {
+
 };
