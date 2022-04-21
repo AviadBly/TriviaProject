5387ca1c1b80376ffe2f18bf36c545f6a6c63e9f
shahar m <shaharmar1@kramim.ort.org.il>
2022-04-21 23:40:37 +0300

    Adding class Client with sender

--- clientAPI/Login and Signup/LoginWindow.xaml.cs
+++ clientAPI/Login and Signup/LoginWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text.Json;
-
+using clientAPI.Requests;
 
 namespace clientAPI
 {
@@ -65,11 +65,19 @@ namespace clientAPI
             //{
             //    return;
             //}
+
             
+
             string username = UsernameText.Text;
             string password = PasswordText.Password.ToString();
-            if (username != null)   
-            Console.WriteLine(username);
+
+            LoginRequest loginRequest = new LoginRequest(username, password);
+            if (username == null || password == null)
+            {
+                return;
+            }   
+
+            Console.WriteLine(loginRequest);
 
             menu menuWindow = new menu();
             menuWindow.Show();
@@ -85,58 +93,6 @@ namespace clientAPI
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool AllocConsole();
 
-        public static NetworkStream? client(String server)
-        {
-            try
-            {
-                // Create a TcpClient.
-                // Note, for this client to work you need to have a TcpServer
-                // connected to the same address as specified by the server, port
-                // combination.
-                Int32 port = 8200;
-                TcpClient client = new TcpClient(server, port);
-
-                // Translate the passed message into ASCII and store it as a Byte array.
-                //Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                // Get a client stream for reading and writing.
-                //  Stream stream = client.GetStream();
-
-                NetworkStream stream = client.GetStream();
-
-                return stream;
-                // Send the message to the connected TcpServer.
-                //stream.Write(data, 0, data.Length);
-
-                //Console.WriteLine("Sent: {0}", message);
-
-                //// Receive the TcpServer.response.
-
-                //// Buffer to store the response bytes.
-                //data = new Byte[256];
-
-                //// String to store the response ASCII representation.
-                //String responseData = String.Empty;
-
-                //// Read the first batch of the TcpServer response bytes.
-                //Int32 bytes = stream.Read(data, 0, data.Length);
-                //responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                //Console.WriteLine("Received: {0}", responseData);
-
-                //// Close everything.
-                //stream.Close();
-                //client.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            return null;
-            //private void loginClick(object sender, RoutedEventArgs args)
-            //{
-            //    client("127.0.0.1", "heello");
-            //}
-        }
+        
     }
 }
