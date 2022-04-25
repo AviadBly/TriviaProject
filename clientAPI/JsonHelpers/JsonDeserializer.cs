8913634797fadb38bebcb2733fc16a0636d6107f
Aviad Blyacher <aviadbl05@gmail.com>
2022-04-25 18:48:09 +0300

    Deserializer responses

---
+++ clientAPI/JsonHelpers/JsonDeserializer.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clientAPI.Requests_and_Responses;
+using System.Text.Json;
+
+namespace clientAPI.JsonHelpers
+{
+    internal class JsonFormatDeserializer
+    {
+        //byte[0] - code, byte[1,2,3,4] - length, rest = jsonstring
+        public static LoginResponse? loginResponseDeserializer(byte[] buffer)
+        {
+            
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+            
+
+            LoginResponse? response = JsonSerializer.Deserialize<LoginResponse>(jsonString);
+
+            return response;
+        }
+
+        public static signUpResponse? signUpResponseDeserializer(byte[] buffer)
+        {
+            
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+           
+            
+            signUpResponse? response = JsonSerializer.Deserialize<signUpResponse>(jsonString);
+
+            return response;
+        }
+
+        public static ErrorResponse? ErrorResponseDeserializer(byte[] buffer)
+        {
+           
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+         
+
+            ErrorResponse? response = JsonSerializer.Deserialize<ErrorResponse>(jsonString);
+
+            return response;
+        }
+
+        public static LogOutResponse? LogOutResponseDeserializer(byte[] buffer)
+        {
+            
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+           
+
+            LogOutResponse? response = JsonSerializer.Deserialize<LogOutResponse>(jsonString);
+
+            return response;
+        }
+
+        public static GetPlayersInRoomResponse? GetPlayersInRoomResponseDeserializer(byte[] buffer)
+        {
+          
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+           
+
+            GetPlayersInRoomResponse? response = JsonSerializer.Deserialize<GetPlayersInRoomResponse>(jsonString);
+
+            return response;
+        }
+
+        public static GetHighScoreResponse? GetHighScoreResponseDeserializer(byte[] buffer)
+        {
+
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+
+
+            GetHighScoreResponse? response = JsonSerializer.Deserialize<GetHighScoreResponse>(jsonString);
+
+            return response;    
+        }
+
+        public static GetPersonalStatsResponse? GetPersonalStatsResponseDeserializer(byte[] buffer)
+        {
+
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+
+
+            GetPersonalStatsResponse? response = JsonSerializer.Deserialize<GetPersonalStatsResponse>(jsonString);
+
+            return response;
+        }
+
+        public static JoinRoomResponse? JoinRoomResponseDeserializer(byte[] buffer)
+        {
+
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+
+
+            JoinRoomResponse? response = JsonSerializer.Deserialize<JoinRoomResponse>(jsonString);
+
+            return response;
+        }
+
+        public static CreateRoomResponse? CreateRoomResponseDeserializer(byte[] buffer)
+        {
+
+
+            string jsonString = System.Text.Encoding.Default.GetString(buffer);
+
+
+            CreateRoomResponse? response = JsonSerializer.Deserialize<CreateRoomResponse>(jsonString);
+
+            return response;
+        }
+
+
+    }
+}
