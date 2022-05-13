ff7ee5b23d59f091f5b6d1adfaceb252317472b1
Aviad Blyacher <aviadbl05@gmail.com>
2022-05-13 19:11:53 +0300

    probably added waiting room refresh

--- clientAPI/Game/WaitingRoom.xaml.cs
+++ clientAPI/Game/WaitingRoom.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,24 +23,69 @@ namespace clientAPI.Game
     /// </summary>
     public partial class WaitingRoom : Window
     {
+        private const int PLAYERS_UPDATE_INTERVAL_SECONDS = 1;
+
         private Room m_room;
+
+        private CancellationTokenSource m_updatePlayersCancellationToken;
+        private Task m_updatePlayersTask;
 
         public WaitingRoom(RoomData metaData)
         {
             InitializeComponent();
 
-            var players = getPlayers();
-
             m_room = new Room(metaData, new List<string>());
             m_room.PlayersUpdated += RoomPlayersUpdated;
+
+            this.Loaded += WaitingRoom_Loaded;
+            this.Closed += WaitingRoom_Closed;
+
+            // Task.Run(TimelyUpdatePlayers);
+        }
+
+        private void WaitingRoom_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (m_updatePlayersCancellationToken != null)
+            {
+                m_updatePlayersCancellationToken?.Dispose();
+                m_updatePlayersCancellationToken = null;
+            }
+
+            m_updatePlayersCancellationToken = new CancellationTokenSource();
+            m_updatePlayersTask = Task.Run(TimelyUpdatePlayers, m_updatePlayersCancellationToken.Token);
+        }
+
+        private void WaitingRoom_Closed(object? sender, EventArgs e)
+        {
+            m_updatePlayersCancellationToken?.Cancel();
+            m_updatePlayersTask.Wait(TimeSpan.FromSeconds(PLAYERS_UPDATE_INTERVAL_SECONDS * 3));
+            
+            m_updatePlayersTask.Dispose();
+            m_updatePlayersTask = null;
+        }
+
+        private void UpdatePlayers()
+        {
+            var players = getPlayers();
             m_room.UpdatePlayers(players);
+        }
+
+        private void TimelyUpdatePlayers()
+        {
+            while (!m_updatePlayersCancellationToken.IsCancellationRequested)
+            {
+                UpdatePlayers();
+                Task.Delay(TimeSpan.FromSeconds(PLAYERS_UPDATE_INTERVAL_SECONDS)).Wait();
+            }
         }
 
         private void RoomPlayersUpdated(IList<string> players)
         {
-            showPlayers(players);
+            if (!Dispatcher.CheckAccess())
+                Dispatcher.Invoke(() => showPlayers(players));
+            else
+                showPlayers(players);
         }
-
 
         //TO DO, ask getPlayers every 1 second
         private IList<string> getPlayers()
